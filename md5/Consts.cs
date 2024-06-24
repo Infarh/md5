579e57d7cf0be09804c6b65624d1aeeba47b9d90
Infarh <shmachilin@gmail.com>
2024-06-24 18:42:56 +0300

    Рефакторинг

---
+++ md5/Consts.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace md5;
+
+internal class Consts
+{
+    public static CultureInfo Culture { get; } = CreateCulture(CultureInfo.InvariantCulture);
+
+    private static CultureInfo CreateCulture(CultureInfo BaseCulture)
+    {
+        var culture = (CultureInfo)BaseCulture.Clone();
+
+        var number_format = culture.NumberFormat;
+        number_format.NumberGroupSeparator = "'";
+
+
+        return culture;
+    }
+}
--- md5/Program.cs
+++ md5/Program.cs
@@ -1,79 +1,101 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
-namespace md5
+if (args is not { Length: > 0 })
+    args = [Environment.CurrentDirectory];
+
+var pause_on_complete = false;
+var logging = false;
+
+foreach (var arg in args)
+    if (File.Exists(arg))
+        ProcessFile(new(arg));
+    else if (Directory.Exists(arg))
+        ProcessDirectory(new(arg));
+    else if (!string.IsNullOrEmpty(arg))
+        Process(arg, ref logging, ref pause_on_complete);
+
+if (!pause_on_complete) 
+    return;
+
+Console.WriteLine("Расчёт сумм завершён.");
+Console.WriteLine("Нажмите Enter для продолжения...");
+Console.ReadLine();
+
+return;
+
+static void ProcessFile(FileInfo file)
 {
-    internal class Program
+    using var data = file.OpenRead();
+    var hash = MD5.HashData(data);
+
+    FormattableString inf = $"{file.Name} ({file.Length:N0}b)";
+    PrintHash(inf.ToString(Consts.Culture), hash);
+}
+
+static void ProcessDirectory(DirectoryInfo directory)
+{
+    Console.WriteLine("dir:{0} - files:{1}", directory.FullName, directory.EnumerateFiles().Count());
+
+    foreach (var file in directory.EnumerateFiles())
+        ProcessFile(file);
+
+    var sub_dirs = directory.GetDirectories();
+
+    if (sub_dirs.Length == 0)
+        return;
+
+    Console.WriteLine("dir:{0} - subdirs:{1}", directory.FullName, sub_dirs.Length);
+
+    foreach (var sub_dir in sub_dirs)
+        ProcessDirectory(sub_dir);
+}
+
+static void Process(string str, ref bool Logging, ref bool PauseOnComplete)
+{
+    if (str is ['-' or '/', ..] && ProcessCommand(str, ref Logging, ref PauseOnComplete))
+        return;
+
+    var hash = MD5.HashData(Encoding.Default.GetBytes(str));
+
+    PrintHash($"str:\"{str}\"", hash);
+}
+
+static void PrintHash(string Source, byte[] hash, string comment = null, bool logging = false)
+{
+    var str = new StringBuilder(Source.Length + 5 + 16 * 2 + (comment is { Length: > 0 } ? comment.Length + 1 : 0));
+    str.Append(Source).Append(" md5:");
+
+    foreach (var b in hash)
+        str.Append($"{b:X2}");
+
+    if (!string.IsNullOrWhiteSpace(comment))
+        str.Append(' ').Append(comment);
+
+    Console.WriteLine(str);
+
+    if (!logging) return;
+    
+    using var log = File.AppendText("md5.log");
+    log.WriteLine(str);
+}
+
+static bool ProcessCommand(string cmd, ref bool Logging, ref bool PauseOnComplete)
+{
+    Logging = false;
+    PauseOnComplete = false;
+
+    if (cmd.EndsWith("log", StringComparison.InvariantCultureIgnoreCase))
     {
-        private static bool __Logging;
-        private static bool __PauseOnComplete;
-
-        private static void Main(string[] args)
-        {
-            if (args is null || args.Length == 0)
-                args = new[] { Environment.CurrentDirectory };
-            foreach (var arg in args)
-            {
-                if (File.Exists(arg)) Process(new FileInfo(arg));
-                else if (Directory.Exists(arg)) Process(new DirectoryInfo(arg));
-                else if (!string.IsNullOrEmpty(arg)) Process(arg);
-            }
-
-            if (!__PauseOnComplete) return;
-            Console.WriteLine("Расчёт сумм завершён.");
-            Console.WriteLine("Нажмите Enter для продолжения...");
-            Console.ReadLine();
-        }
-
-        private static void Process(FileInfo file)
-        {
-            using var md5 = new MD5CryptoServiceProvider();
-            using var data = file.OpenRead();
-            var hash = md5.ComputeHash(data);
-            PrintHash($"{file.Name} ({file.Length}b)", hash);
-        }
-
-        private static void Process(DirectoryInfo directory)
-        {
-            Console.WriteLine("dir:{0} - files:{1}", directory.FullName, directory.EnumerateFiles().Count());
-            foreach (var file in directory.EnumerateFiles())
-                Process(file);
-            var sub_dirs = directory.GetDirectories();
-            if (sub_dirs.Length == 0) return;
-            Console.WriteLine("dir:{0} - subdirs:{1}", directory.FullName, sub_dirs.Length);
-            foreach (var sub_dir in sub_dirs)
-                Process(sub_dir);
-        }
-
-        private static void Process(string str)
-        {
-            if (str.StartsWith("-") || str.StartsWith("/") && ProcessCommand(str)) return;
-            using var md5 = new MD5CryptoServiceProvider();
-            var hash = md5.ComputeHash(Encoding.Default.GetBytes(str));
-
-            PrintHash($"str:\"{str}\"", hash);
-        }
-
-        private static void PrintHash(string Source, byte[] hash, string comment = null)
-        {
-            var str = $"{Source} - {string.Join("", hash.Select(b => b.ToString("X2")))}";
-            if (!string.IsNullOrWhiteSpace(comment)) str += $" {comment}";
-            Console.WriteLine(str);
-            if (!__Logging) return;
-            using var log = File.AppendText("md5.log");
-            log.WriteLine(str);
-        }
-
-        private static bool ProcessCommand(string cmd)
-        {
-            if (cmd.EndsWith("log", StringComparison.InvariantCultureIgnoreCase))
-                return __Logging = true;
-            if (cmd.EndsWith("pause", StringComparison.InvariantCultureIgnoreCase))
-                return __PauseOnComplete = true;
-            return false;
-        }
+        Logging = true;
+        return true;
     }
+
+    if (cmd.EndsWith("pause", StringComparison.InvariantCultureIgnoreCase))
+    {
+        PauseOnComplete = true;
+        return true;
+    }
+
+    return false;
 }
